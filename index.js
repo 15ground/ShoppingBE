62e2a14fa058312cabb3356f0c56c479654ddc0d
jhun1211 <lvhungbk@gmail.com>
2021-09-03 15:16:14 +0700

    update

--- index.js
+++ index.js
@@ -11,6 +11,15 @@ const app = express();
 // Middleware
 app.use(express.json());
 
+// add header cho phép các trang có domain khác với domain của backend gọi được api
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Methods: GET, POST, PUT, PATCH, DELETE, HEAD, OPTIONS');
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+})
+
+
 // Router
 app.use("/api/products", require("./routes/products"));
 
